1eb1026efac8cec301a17e28bb3d25a253ada20d
sanjeev3 <sanjeev.themegrill@gmail.com>
2018-01-31 17:16:06 +0545

    Spacings and standards

--- class-BetaTesterSettingsPage.php
+++ class-BetaTesterSettingsPage.php
@@ -2,30 +2,27 @@
 class BetaTesterSettingsPage
 {
     public function __construct() {
-            add_action( 'admin_menu', array($this, 'beta_tester_plugin_menu' ));
+            add_action( 'admin_menu', array( $this, 'beta_tester_plugin_menu' ) );
           
     }
     public function beta_tester_plugin_menu() {
          add_submenu_page( 'index.php','Beta Tester Settings', 'Beta Tester Settings', 'manage_options', 'beta-tester-settings-page', array($this, 'betatester_callback' ) );
     }
     public function betatester_callback() {
-        if ( !current_user_can( 'manage_options' ) )  {
+        if ( !current_user_can( 'manage_options' ) ) {
             wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
         }
 
-        if(isset($_POST['submit']))
-        {
-            $plugin=isset($_POST['plugin'])?$_POST['plugin']:null;
-            update_option('tgbt_plugin_field',$plugin);
+        if( isset( $_POST['submit']) ) {
+            $plugin = isset( $_POST['plugin'] )?$_POST['plugin']:null;
+            update_option( 'tgbt_plugin_field', $plugin );
             echo "Settings Updated!!";
         }
            
             ?>
                 <form method="post" action=" <?php echo  $_SERVER['REQUEST_URI'] ;?>">
-
                     <label>Github Repository Owner:</label>
                     <br/>
-
                     <label>Plugin For Beta Test</label>
                     <select name="plugin">
                         <option <?php if ( get_option( 'tgbt_plugin_field' ) == "user-registration") echo "selected='selected'";?> >user-registration</option>
@@ -33,7 +30,6 @@ class BetaTesterSettingsPage
                         <option <?php if ( get_option( 'tgbt_plugin_field' ) == "online-restaurant-reservation") echo "selected='selected'";?> >online-restaurant-reservation</option>
                         <option <?php if ( get_option( 'tgbt_plugin_field' ) == "everest-forms") echo "selected='selected'";?> >everest-forms</option>
                     </select><br>
-
                     <input type="submit" name="submit">
                 </form>         
             <?php
